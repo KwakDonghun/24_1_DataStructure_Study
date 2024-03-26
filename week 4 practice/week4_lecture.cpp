8e62b13a30ddabe8260ed633e126d0fca8d5029a
KwakDonghun <81298330+KwakDonghun@users.noreply.github.com>
2024-03-26 20:44:34 +0900

    Add files via upload

---
+++ week 4 practice/week4_lecture.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+
+class vector {
+private:
+	int* arr;
+	int index;
+	int capacity;
+	int f;
+	int r;
+public:
+	vector(int c){
+		capacity = c;
+		arr = new int[capacity];
+		index = -1;
+		f = 0; //환형에서 index 0 인 곳
+		r = 0; //환형에서 마지막 값 다음 index
+	}
+	int size() {
+		return index + 1;
+	}
+	bool empty() {
+		if (size() == 0)
+			return true;
+		else
+			return false;
+	}
+	int at(int in) {
+		if (in >= size())
+			return -1;
+		else
+			return arr[in];
+	}
+	void insert(int i, int o) {
+		if (i > size())
+			return;
+		if (size() + 1 > capacity) {
+			int* arr2 = new int[2 * capacity];
+			for (int j = 0; j < size(); j++) {
+				arr2[j] = arr[(f + j) % capacity];
+			}
+			capacity *= 2;
+			f = 0;
+			r = size();
+			arr = arr2; //class에 있는 arr의 주소값을 arr2로 교체하는 방법이 이게 맞는가? 
+		}
+		if (i == 0) {
+			f = (f - 1 + capacity) % capacity;
+			arr[f] = o;
+			return;
+		}
+
+		// 보편적인 방법에서 insert 하는 법 기입
+	}
+	void erase(int in) {
+		// 환형 방법에서 erase 하는 법 기입
+	}
+};
+
---
+++ week 4 practice/week4_practice_queue.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class arrayqueue {
+private:
+	int* arr;
+	int frontindex;
+	int rearindex;
+	int capacity;
+	int size;
+public:
+	arrayqueue(int c) {
+		capacity = c;
+		arr = new int[capacity];
+		frontindex = rearindex = 0;
+		size = 0;
+	}
+	bool empty() {
+		if (size == 0)
+			return true;
+		return false;
+	}
+	int getsize() {
+		return size;
+	}
+	int front() {
+		if (empty())
+			return -1;
+		return arr[frontindex];
+	}
+	void enqueue(int data) {
+		if (size >= capacity)
+			return;
+		arr[rearindex] = data;
+		rearindex = (rearindex + 1) % capacity;
+		size++;
+	}
+	void dequeue() {
+		if (empty())
+			return;
+		frontindex=(frontindex + 1) % capacity;
+		size--;
+	}
+};
+class node {
+private:
+	int data;
+	node* next;
+public:
+	node(int val){
+		data = val;
+		next = NULL;
+	}
+	friend class listqueue;
+};
+class listqueue {
+private:
+	node* frontnode;
+	node* rearnode;
+	int size;
+public:
+	listqueue() {
+		frontnode = NULL;
+		rearnode = NULL;
+		size = 0;
+	}
+	bool empty() {
+		if (size == 0)
+			return true;
+		else
+			return false;
+	}
+	int getsize() {
+		return size;
+	}
+	int front() {
+		if (empty())
+			return -1;
+		return frontnode->data;
+	}
+	void enqueue(int val) {
+		node* newnode = new node(val);
+		if (empty())
+		{
+			newnode = frontnode;
+			frontnode->next = rearnode;
+		}
+		else {
+			newnode = rearnode;
+			rearnode = rearnode->next;
+		}
+		size++;
+	}
+	void dequeue() {
+		if (empty())
+			return;
+		node* del = frontnode;
+		frontnode = frontnode->next;
+		delete del;
+		size--;
+	}
+};
