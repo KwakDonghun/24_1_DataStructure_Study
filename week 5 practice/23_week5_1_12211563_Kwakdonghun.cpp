1079bda8d1f2dc0ef3cb2c007bfcc2e709a6576f
KwakDonghun <81298330+KwakDonghun@users.noreply.github.com>
2024-04-03 20:25:20 +0900

    Add files via upload

---
+++ week 5 practice/23_week5_1_12211563_Kwakdonghun.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class node {
+public:
+	int val;
+	node* next;
+	node* prev;
+	node(int v) {
+		val = v;
+		next = prev = NULL;
+	}
+};
+
+class doublylist {
+private:
+	node* header;
+	node* trailer;
+	int size;
+	node* p;
+public:
+	doublylist() {
+	
+		header = new node(0);
+		trailer = new node(0);
+		header->next = trailer;
+		trailer->prev = header;
+		begin();
+		header->prev = trailer->next = NULL;
+		size = 0;
+	}
+	bool empty() {
+		return (size == 0);
+	}
+	void begin() {
+		p = header->next;
+	}
+	void end() {
+		p = trailer;
+	}
+	void insert(int e) {
+		node* newnode = new node(e);
+		newnode->next = p;
+		newnode->prev = p->prev;
+		p->prev->next = newnode;
+		p->prev = newnode;
+		size++;
+	}
+	void erase() {
+		if (empty())
+		{
+			cout << "empty\n";
+			return;
+		}
+		if (p == trailer)
+			return;
+		node* del = p;
+		del->prev->next = del->next;
+		del->next->prev = del->prev;
+		begin();
+		delete del;
+		size--;
+	}
+	void nextp() {
+		if (p == trailer)
+			return;
+		p = p->next;
+	}
+	void prevp() {
+		if (p == header->next)
+			return;
+		p = p->prev;
+	}
+	void findmultiple(int e) {
+		if (empty()) {
+			cout << -1 << '\n';
+			return;
+		}
+		int correct_index = 0;
+		begin();
+		for (int index=0; index < size; index++) {
+			if ((p->val) % e == 0) {
+				cout << index << ' ';
+				correct_index++;
+			}
+			nextp();
+		}
+		if (correct_index == 0) {
+			cout << -1 << '\n';
+			return;
+		}
+		cout << '\n';
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int command_num, input;
+	cin >> command_num;
+	doublylist list;
+	for (int i = 0; i < command_num; i++) {
+		string command;
+		cin >> command;
+		if (command == "findmultiple") {
+			cin >> input;
+			list.findmultiple(input);
+		}
+		else if (command == "erase") {
+			list.erase();
+		}
+		else if (command == "insert") {
+			cin >> input;
+			list.insert(input);
+		}
+		else if (command == "prevP") {
+			list.prevp();
+		}
+		else if (command == "nextP") {
+			list.nextp();
+		}
+		else if (command == "begin") {
+			list.begin();
+		}
+		else if (command == "end") {
+			list.end();
+		}
+	}
+}
---
+++ week 5 practice/23_week5_4_12211563_Kwakdonghun.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class node {
+public:
+	int val;
+	node* next;
+	node* prev;
+	node(int v) {
+		val = v;
+		next = prev = NULL;
+	}
+};
+
+class doublylist {
+private:
+	node* header;
+	node* trailer;
+	int size;
+	node* p;
+public:
+	doublylist() {
+
+		header = new node(0);
+		trailer = new node(0);
+		header->next = trailer;
+		trailer->prev = header;
+		begin();
+		header->prev = trailer->next = NULL;
+		size = 0;
+	}
+	bool empty() {
+		return (size == 0);
+	}
+	void begin() {
+		p = header->next;
+	}
+	void end() {
+		p = trailer;
+	}
+	void insert(int e) {
+		node* newnode = new node(e);
+		newnode->next = p;
+		newnode->prev = p->prev;
+		p->prev->next = newnode;
+		p->prev = newnode;
+		size++;
+	}
+	void erase() {
+		if (empty())
+		{
+			cout << "empty\n";
+			return;
+		}
+		if (p == trailer)
+			return;
+		node* del = p;
+		del->prev->next = del->next;
+		del->next->prev = del->prev;
+		begin();
+		delete del;
+		size--;
+	}
+	void nextp() {
+		if (p == trailer)
+			return;
+		p = p->next;
+	}
+	void prevp() {
+		if (p == header->next)
+			return;
+		p = p->prev;
+	}
+	void findmultiple(int e) {
+		if (empty()) {
+			cout << -1 << '\n';
+			return;
+		}
+		int correct_index = 0;
+		begin();
+		for (int index = 0; index < size; index++) {
+			if ((p->val) % e == 0) {
+				cout << index << ' ';
+				correct_index++;
+			}
+			nextp();
+		}
+		if (correct_index == 0) {
+			cout << -1 << '\n';
+			return;
+		}
+		cout << '\n';
+	}
+
+	int pval() {
+		if (empty())
+			return -1;
+		return p->val;
+	}
+
+	void discrimination() {
+		int div = size / 2;
+		if (size % 2 == 1) {
+			node* cur = header->next;
+			for (int i = 0; i < div; i++)
+				cur = cur->next;
+			if (cur == p) {
+				cout << "middle\n";
+				return;
+			}
+		}
+		node* cur = header->next;
+		for (int i = 0; i < div; i++) {
+			if (p == cur) {
+				cout << "header\n";
+				return;
+			}
+			cur = cur->next;
+		}
+		cout << "trailer\n";
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int test_case, N, input, command;
+	cin >> test_case;
+	for (int i = 0; i < test_case; i++) {
+		doublylist game;
+		cin >> N;
+		for (int j = 0; j < N; j++) {
+			cin >> input;
+			game.insert(input);
+		}
+		game.begin();
+		cin >> command;
+		for (int j = 0; j < command; j++) {
+			cin >> input;
+			if (input > 0) {
+				for (int k = 0; k < input; k++)
+					game.nextp();
+			}
+			else {
+				for (int k = 0; k < (-input); k++)
+					game.prevp();
+			}
+			input = game.pval();
+			if (input > 0) {
+				for (int k = 0; k < input; k++)
+					game.nextp();
+			}
+			else {
+				for (int k = 0; k < (-input); k++)
+					game.prevp();
+			}
+		}
+		game.discrimination();
+	}
+}
