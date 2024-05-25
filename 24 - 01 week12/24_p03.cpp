ef2305fefe1c3354918928d0ab91db36a60f3542
KwakDonghun <81298330+KwakDonghun@users.noreply.github.com>
2024-05-25 09:53:46 +0900

    Add files via upload

--- 24 - 01 week12/24_p03.cpp
+++ 24 - 01 week12/24_p03.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>	
 #include <string>
 
 using namespace std;
@@ -11,82 +11,73 @@ struct entry {
 	int key, valid;
 	string value;
 	entry() {
-		key = 0;
+		key = valid = no_item;
 		value = "";
-		valid = available;
 	}
 	entry(int k, string s) {
 		key = k;
 		value = s;
 		valid = is_item;
 	}
-	void erase() {
-		this->valid = available;
-	}
 };
-
 class hashtable {
 public:
-	entry* table;
 	int capacity;
-	int divisor;
-	int hash1(int key) {
-		return key % capacity;
+	entry* table;
+	int hash(int k) {
+		return k % capacity;
 	}
-	int hash2(int key) {
-		return divisor - (key % divisor);
-	}
-	hashtable(int n, int m) {
+	hashtable(int n) {
 		capacity = n;
-		divisor = m;
 		table = new entry[capacity];
 	}
 	void put(int k, string s) {
-		int idx = hash1(k);
+		int idx = hash(k);
 		int cnt = 1;
 		while (table[idx].valid == is_item && cnt <= capacity) {
-			idx = hash1(idx + hash2(k));
+			idx = hash(idx + 1);
 			cnt++;
 		}
-		if (cnt > capacity) {
-			cout << -1 << '\n';
-		}
-		else {
+		if (table[idx].valid != is_item) {
 			table[idx] = entry(k, s);
-			cout << hash1(k)<< ' ' << idx << '\n';
+			cout << hash(k) << ' ' << idx << '\n';
+			return;
 		}
+		cout << -1 << '\n';
 	}
 	void erase(int k) {
-		int idx = hash1(k);
+		int idx = hash(k);
 		int cnt = 1;
 		while (table[idx].valid != no_item && cnt <= capacity) {
 			if (table[idx].valid == is_item && table[idx].key == k) {
-				table[idx].erase();
 				cout << table[idx].value << '\n';
+				table[idx].valid = available;
 				return;
 			}
-			idx = hash1(idx + hash2(k));
+			idx = hash(idx + 1);
 			cnt++;
 		}
 		cout << "None\n";
 	}
-	string find(int k) {
-		int idx = hash1(k);
+	void find(int k) {
+		int idx = hash(k);
 		int cnt = 1;
 		while (table[idx].valid != no_item && cnt <= capacity) {
-			if (table[idx].valid == is_item && table[idx].key == k)
-				return table[idx].value;
-			idx = hash1(idx + hash2(k));
+			if (table[idx].valid == is_item && table[idx].key == k) {
+				cout << table[idx].value << '\n';
+				return;
+			}
+			idx = hash(idx + 1);
 			cnt++;
 		}
-		return "None";
+		cout << "None\n";
 	}
 	void vacant() {
-		int result = 0;
+		int cnt = 0;
 		for (int i = 0; i < capacity; i++)
 			if (table[i].valid != is_item)
-				result++;
-		cout << result << '\n';
+				cnt++;
+		cout << cnt << '\n';
 	}
 };
 
@@ -95,10 +86,10 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int T, k, N, M;
+	int T, k, N;
 	string cmd, s;
-	cin >> T >> N >> M;
-	hashtable* hs = new hashtable(N,M);
+	cin >> T >> N ;
+	hashtable* hs = new hashtable(N);
 	while (T--) {
 		cin >> cmd;
 		if (cmd == "put") {
@@ -107,7 +98,7 @@ int main() {
 		}
 		else if (cmd == "find") {
 			cin >> k;
-			cout << hs->find(k) << '\n';
+			hs->find(k);
 		}
 		else if (cmd == "erase") {
 			cin >> k;
@@ -117,5 +108,4 @@ int main() {
 			hs->vacant();
 		}
 	}
-
 }
