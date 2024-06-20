29306b2880557982ec0f7670def60ea09e1886e7
KwakDonghun <81298330+KwakDonghun@users.noreply.github.com>
2024-06-20 14:35:56 +0900

    Add files via upload

--- 24 - 01 week10/week10_p03.cpp
+++ 24 - 01 week10/week10_p03.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -6,22 +6,22 @@ using namespace std;
 class heapPQ {
 public:
 	vector<int> pq;
-	int root, last;
+	int root = 1, last;
 	void swap(int idx1, int idx2) {
 		int temp = pq[idx1];
 		pq[idx1] = pq[idx2];
 		pq[idx2] = temp;
 	}
 	bool compare(int idx1, int idx2) {
-		return pq[idx1] < pq[idx2];
+		return pq[idx1] > pq[idx2];
 	}
 	heapPQ() {
 		pq.clear();
-		root = 1, last = 0;
+		last = 0;
 		pq.push_back(0);
 	}
-	bool empty() { return (last == 0); }
 	int size() { return last; }
+	bool empty() { return (last == 0); }
 	void upheap(int idx) {
 		if (idx == root) return;
 		int par = idx / 2;
@@ -30,13 +30,14 @@ public:
 			upheap(par);
 		}
 	}
-	void push(int i) {
+	void push(int e) {
 		last++;
-		pq.push_back(i);
+		pq.push_back(e);
 		upheap(last);
 	}
 	void downheap(int idx) {
-		int lchild = 2 * idx, rchild = 2 * idx + 1;
+		int lchild = 2 * idx;
+		int rchild = 2 * idx + 1;
 		if (rchild <= last) {
 			int prior;
 			compare(lchild, rchild) ? prior = lchild : prior = rchild;
@@ -52,36 +53,28 @@ public:
 			}
 		}
 	}
-	void top() {
-		if (empty()) {
-			cout << -1 << '\n';
-			return;
-		}
-		cout << pq[root] << '\n';
-	}
-	void pop() {
-		if (empty()) {
-			cout << -1 << '\n';
-			return;
-		}
-		cout << pq[root] << '\n';
+	int pop() {
+		if (empty()) return -1;
+		int top_val = pq[root];
 		pq[root] = pq[last];
 		pq.erase(pq.begin() + last);
 		last--;
 		downheap(root);
+		return top_val;
+	}
+	int top() {
+		if (empty()) return -1;
+		return pq[root];
 	}
 	void second_top() {
-		if (last < 2) {
+		if (size() < 2) {
 			cout << "Error\n";
 			return;
 		}
-		else if (last == 2)
-			cout << pq[2] << '\n';
-		else {
-			if (compare(2, 3)) cout << pq[2] << '\n';
-			else cout << pq[3] << '\n';
-		}
-		
+		int top_val = pq[root];
+		pop();
+		cout << top() << '\n';
+		push(top_val);
 	}
 };
 
@@ -92,22 +85,22 @@ int main() {
 	cin >> N;
 	while (N--) {
 		cin >> cmd;
-		if (cmd == "empty") {
+		if (cmd == "size") {
+			cout << pq.size() << '\n';
+		}
+		else if (cmd == "empty") {
 			if (pq.empty()) cout << 1 << '\n';
 			else cout << 0 << '\n';
-		}
-		else if (cmd == "size") {
-			cout << pq.size() << '\n';
 		}
 		else if (cmd == "push") {
 			cin >> input;
 			pq.push(input);
 		}
 		else if (cmd == "pop") {
-			pq.pop();
+			cout << pq.pop() << '\n';
 		}
 		else if (cmd == "top") {
-			pq.top();
+			cout << pq.top() << '\n';
 		}
 		else if (cmd == "second_top") {
 			pq.second_top();
---
+++ 24 - 01 week10/week10_p04.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class heapPQ {
+public:
+	vector<int> pq;
+	int root = 1, last;
+	void swap(int lhs, int rhs) {
+		int temp = pq[lhs];
+		pq[lhs] = pq[rhs];
+		pq[rhs] = temp;
+	}
+	bool compare(int lhs, int rhs) {
+		if (pq[lhs] % 2 == pq[rhs] % 2) {
+			if (pq[lhs] % 2 == 1) return pq[lhs] > pq[rhs];
+			else return pq[lhs] < pq[rhs];
+		}
+		else {
+			return pq[lhs] % 2 == 0;
+		}
+	}
+	heapPQ() {
+		pq.clear();
+		last = 0;
+		pq.push_back(0);
+	}
+	bool empty() { return (last == 0); }
+	int size() {return last;}
+	void upheap(int idx) {
+		if (idx == root) return;
+		int par = idx / 2;
+		if (compare(idx, par)) {
+			swap(idx, par);
+			upheap(par);
+		}
+	}
+	void push(int e) {
+		last++;
+		pq.push_back(e);
+		upheap(last);
+	}
+	void downheap(int idx) {
+		int lchild = 2 * idx, rchild = 2 * idx + 1;
+		if (rchild <= last) {
+			int prior;
+			compare(lchild, rchild) ? prior = lchild : prior = rchild;
+			if (compare(prior, idx)) {
+				swap(prior, idx);
+				downheap(prior);
+			}
+		}
+		else if (lchild <= last) {
+			if (compare(lchild, idx)) {
+				swap(lchild, idx);
+				downheap(lchild);
+			}
+		}
+	}
+	int top() {
+		if (empty()) return -1;
+		else return pq[root];
+	}
+	int pop() {
+		if (empty()) return -1;
+		int top_val = pq[root];
+		pq[root] = pq[last];
+		pq.pop_back();
+		last--;
+		downheap(root);
+		return top_val;
+	}
+};
+int main() {
+	int T, N, input;
+	cin >> T;
+	while (T--) {
+		heapPQ pq;
+		cin >> N;
+		for (int i = 0; i < N; i++) {
+			cin >> input;
+			pq.push(input);
+		}
+		for (int i = 0; i < N; i++) {
+			cout << pq.pop() << ' ';
+		}
+		cout << '\n';
+	}
+}
